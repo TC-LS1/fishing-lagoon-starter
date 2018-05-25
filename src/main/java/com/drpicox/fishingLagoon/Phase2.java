d82243e587a5752d2c459ad5da55c83e0a7d3369
David Rodenas <david.rodenas+git@gmail.com>
2018-05-25 07:55:50 +0200

    convert store into a store that uses SQL

--- src/main/java/com/drpicox/fishingLagoon/Phase2.java
+++ src/main/java/com/drpicox/fishingLagoon/Phase2.java
@@ -7,14 +7,18 @@ import com.drpicox.fishingLagoon.phase2.presentation.RemoteCombatController;
 import com.drpicox.fishingLagoon.phase2.business.TitForTatStrategy;
 
 import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Phase2 {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException, SQLException {
         var serverUrl = System.getenv("FISHING_LAGOON_HOST_URL");
         var botToken = System.getenv("FISHING_LAGOON_BOT_TOKEN");
+        var connectionUrl = System.getenv("FISHING_LAGOON_CONNECTION_URL");
 
-        var traitorsStore = new TraitorsStore();
+        var connection = DriverManager.getConnection(connectionUrl);
+        var traitorsStore = new TraitorsStore(connection);
         var myStrategy = new TitForTatStrategy(traitorsStore);
 
         var actionParser = new ActionParser();
--- src/main/java/com/drpicox/fishingLagoon/phase2/persistence/TraitorsStore.java
+++ src/main/java/com/drpicox/fishingLagoon/phase2/persistence/TraitorsStore.java
@@ -2,19 +2,53 @@ package com.drpicox.fishingLagoon.phase2.persistence;
 
 import com.drpicox.fishingLagoon.bots.BotId;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
 public class TraitorsStore {
 
-    private Set<BotId> traitors = new HashSet<>();
+    private Connection connection;
+
+    public TraitorsStore(Connection connection) throws SQLException {
+        this.connection = connection;
+
+        try (var stmt = connection.createStatement()) {
+            stmt.execute("" +
+                    "CREATE TABLE IF NOT EXISTS traitors (" +
+                    "  traitorId VARCHAR(255)," +
+                    "  PRIMARY KEY (traitorId)" +
+                    ")"
+            );
+        }
+    }
 
     public Set<BotId> list() {
-        return new HashSet<>(traitors);
+        var result = new HashSet<BotId>();
+
+        try (var stmt = connection.prepareStatement("SELECT * FROM traitors")) {
+            try (var rows = stmt.executeQuery()) {
+                while (rows.next()) {
+                    String traitorId = rows.getString("traitorId");
+                    result.add(new BotId(traitorId));
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        return result;
     }
 
     public void save(BotId traitorId) {
-        traitors.add(traitorId);
+        try (var stmt = connection.prepareStatement("MERGE INTO traitors(traitorId) VALUES (?)")) {
+            stmt.setString(1, traitorId.getValue());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
